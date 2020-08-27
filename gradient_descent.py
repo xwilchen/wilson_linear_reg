c7cec5068755f7a4ec7f114284276823aa0fecad
xchen715 <wilsonhc715@gmail.com>
2020-08-26 23:36:13 -0400

    tested all functions in GradientDescent

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,36 +1,33 @@
 import numpy as np
 
 class Gradient_Descent():
-    def __init__(self,alpha=0.1,theta=None,X=None,y=None,iteration=1000):
-        self.alpha = alpha
-        self.theta = self.transform_vector(theta)
+    def __init__(self,X,y):
         self.X = self.prepare_X(X)
+        self.theta = np.ones((self.X.shape[1],1)) * 0.5
         self.y = self.transform_vector(y)
-        self.iteration = iteration
-        self.J = None
-
+        self.alpha = 0.1
+        self.J = self.objective_func(self.X,self.theta,self.y)
+    
     def prepare_X(self,X):
         theta_X0 = np.ones((X.shape[0],1))
         return np.concatenate((theta_X0,X),axis=1)
 
     def transform_vector(self,arr):
-        if arr.shape[1] != 1:
-            return arr.T
-        return arr
+        arr = np.array(arr)
+        return arr.reshape((-1,1))
 
     def calculate_delta(self,X,theta,y):
-        return 1/X.shape[0] * (X.dot(theta) - y).T.dot(X)
+        return (1/X.shape[0] * (X.dot(theta) - y).reshape((1,-1)).dot(X)).reshape((-1,1))
 
     def update_theta(self,theta,alpha,X,y):
-        return theta - alpha * 1/len(y) * self.calculate_delta(X,theta,y)
+        return theta - alpha * 1/X.shape[0] * self.calculate_delta(X,theta,y)
 
     def objective_func(self,X,theta,y):
-        return 1/(2*len(y)) * (X.dot(theta) - y).T.dot((X.dot(theta) - y))
+        return 1/(2*X.shape[0]) * (X.dot(theta) - y).reshape((1,-1)).dot((X.dot(theta) - y))
 
     def gradient_descent(self):
-        for i in range(self.iteration):
-            previous_theta = self.theta
+        for i in range(100000):
             self.theta = self.update_theta(self.theta,self.alpha,self.X,self.y)
-            self.J = self.objective_func(self.X,self.theta,self.y)
-            if abs(previous_theta - self.theta) < 0.0001:
-                return (self.theta,self.J)
+            self.J = self.objective_func(self.X, self.theta, self.y)
+            if self.J <= 0.0000001:
+                return np.round(self.theta,2)
